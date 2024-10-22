0d7a6fb23295d5bc0735d23ad39bac12dc477152
jagadish <jagadish.nadar41@gmail.com>
2024-10-23 04:55:49 +0530

    first commit

--- Idea_Labs_ByJagadish/utils/audio_sync_utils.py
+++ Idea_Labs_ByJagadish/utils/audio_sync_utils.py
@@ -7,16 +7,8 @@ from pydub.silence import detect_silence
 
 async def insert_silences_into_ai_audio(original_audio_path, ai_audio_path, audio_output_filepath):
 
-    uploads_dir = Path(__file__).parent.parent / 'uploads'
-    if os.path.exists(uploads_dir):
-        logger.info("it presents")
-    logger.info(f"{os.listdir(uploads_dir)}")
-    ai_audio_full_path = uploads_dir / Path(ai_audio_path).name
-    original_audio_full_path = uploads_dir / Path(original_audio_path).name
-    logger.info(ai_audio_full_path)
-    logger.info(original_audio_full_path)
-    original_audio = AudioSegment.from_wav(original_audio_full_path)
-    ai_audio = AudioSegment.from_wav(ai_audio_full_path)
+    original_audio = AudioSegment.from_wav(original_audio_path)
+    ai_audio = AudioSegment.from_wav(ai_audio_path)
     
     silence_ranges_original = detect_silence(original_audio, min_silence_len=500, silence_thresh=-40)
     silence_ranges_ai = detect_silence(ai_audio, min_silence_len=500, silence_thresh=-40)
